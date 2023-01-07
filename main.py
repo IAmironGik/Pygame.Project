8be50dacb865935ef7797f5a17d693f4e1c18472
IAmiron <geortechov375@gmail.com>
2023-01-07 17:21:52 +0300

    v.1.3

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import math
 import random
+import os
+import sys
 
 import pygame
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
 
 BLACK = "#000000"
 WHITE = "#FFFFFF"
@@ -14,6 +34,7 @@ FPS = 60
 class Bullet:
     def __init__(self, mouse_coords, hero_pos, spread):
         self.is_died = False
+        self.damage = 10
         self.color = WHITE
         self.coords = hero_pos
         self.size = 5
@@ -40,15 +61,25 @@ class Bullet:
 
 
 class Enemy:
-    def __init__(self, coords):
+    def __init__(self, coords, health, speed):
         self.is_died = False
-        self.v = 100
+        self.v = speed
+        self.health = health
         self.coords = coords
         self.color = RED
         self.size = 10
         self.radius = self.size
         x, y = self.coords
         self.rect = pygame.Rect(x, y, 2 * self.size, 2 * self.size)
+
+    def update(self):
+        collide_with = self.rect.collidelist([i.rect for i in bullet_list])
+        if collide_with != -1:
+            bull = bullet_list[collide_with]
+            bullet_list.pop(collide_with)
+            self.health -= bull.damage
+            if self.health <= 0:
+                self.is_died = True
 
     def move(self, player_coords):
         x, y = self.coords
@@ -100,11 +131,17 @@ if __name__ == '__main__':
 
     enemy_list = []
     bullet_list = []
+
     main_hero = Hero(130)
     mouse_coord = (0, 0)
-    running = True
+
+    killed_enemy = 0
+    enemy_health = 30
+    enemy_speed = 100
+
     direction_x = ""
     direction_y = ""
+    running = True
     while running:
         screen.fill(BLACK)
 
@@ -114,7 +151,6 @@ if __name__ == '__main__':
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 bullet_list.append(Bullet(event.pos, main_hero.coords, 0))
-                print(bullet_list[0].coords, bullet_list[0].rect.x, bullet_list[0].rect.y)
 
             if event.type == enemy_upgrade_timer:
                 pygame.time.set_timer(enemy_spawn_timer, 500)
@@ -133,7 +169,8 @@ if __name__ == '__main__':
                 else:
                     y = random.randint(10, HEIGHT - 10)
                     x = 10
-                enemy_list.append(Enemy((x, y)))
+                enemy_list.append(Enemy((x, y), enemy_health, enemy_speed))
+
             key_events = pygame.key.get_pressed()
 
             if key_events:
@@ -156,20 +193,24 @@ if __name__ == '__main__':
 
         main_hero.move(direction_x, direction_y)
         pygame.draw.circle(screen, main_hero.color, main_hero.coords, main_hero.size)
-        for bullet in bullet_list:
+
+        for index, bullet in enumerate(bullet_list):
             pygame.draw.circle(screen, bullet.color, bullet.coords, bullet.size)
             bullet.move()
             if bullet.is_died:
-                bullet_list.remove(bullet)
+                bullet_list.pop(index)
+
         for enemy in enemy_list:
             pygame.draw.circle(screen, enemy.color, enemy.coords, enemy.size)
             enemy.move(main_hero.coords)
-            collide_with = enemy.rect.collidelist([i.rect for i in bullet_list])
-            if collide_with != -1:
-                bullet_list[collide_with].is_died = True
-                enemy.is_died = True
+            enemy.update()
             if enemy.is_died:
                 enemy_list.remove(enemy)
+                killed_enemy += 1
+                # if killed_enemy == 20:
+                #     killed_enemy = 0
+                #     enemy_health += 10
+
         pygame.display.flip()
         clock.tick(FPS)
     pygame.quit()
