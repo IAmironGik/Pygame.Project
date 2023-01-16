935247e24c5f9cdc882bc1fedf446ddccd94e2ac
IAmiron <geortechov375@gmail.com>
2023-01-16 21:11:47 +0300

    v.1.6.2

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import math
 import os
 import random
 import sys
-
 import pygame
 
 
@@ -13,6 +12,7 @@ WIDTH = 1200
 HEIGHT = 800
 FPS = 60
 GRAY = (150, 150, 150)
+soul_bar_font = pygame.font.match_font('arial')
 souls = 0
 hero_health = 0
 get_damage_cooldown = 1000
@@ -24,16 +24,17 @@ enemy_list = []
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = pygame.image.load(fullname)
+
     if colorkey is not None:
         image = image.convert()
         if colorkey == -1:
             colorkey = image.get_at((0, 0))
         image.set_colorkey(colorkey)
+
     else:
         image = image.convert_alpha()
     return image
@@ -63,8 +64,10 @@ class Bullet:
         x += self.vx
         y += self.vy
         self.coords = x, y
+
         if not 0 <= x <= WIDTH or not 0 <= y <= HEIGHT:
             self.is_died = True
+
         self.rect = pygame.Rect(x, y, 2 * self.size, 2 * self.size)
 
 
@@ -84,10 +87,12 @@ class Enemy(pygame.sprite.Sprite):
 
     def update(self, hero):
         self.move((hero.rect.x, hero.rect.y))
+
         collide_with = self.rect.collidelist([i.rect for i in bullet_list])
         if collide_with != -1:
             bull = bullet_list[collide_with]
             bullet_list.pop(collide_with)
+
             self.health -= bull.damage
             if self.health <= 0:
                 self.is_died = True
@@ -124,6 +129,7 @@ class Hero(pygame.sprite.Sprite):
 
     def update(self, *args):
         global previous_getting
+
         for enemy in enemy_list:
             collide_with = pygame.sprite.collide_mask(self, enemy)
             if collide_with:
@@ -131,6 +137,7 @@ class Hero(pygame.sprite.Sprite):
                     self.health -= 1
                     if self.health <= 0:
                         self.is_died = True
+
                     previous_getting = pygame.time.get_ticks()
 
     def move(self, dx, dy):
@@ -157,11 +164,9 @@ class SoulsDrop(pygame.sprite.Sprite):
     def update(self, hero):
         if self.rect.colliderect(hero.rect):
             global souls
+
             souls += 1
             self.kill()
-
-
-soul_bar_font = pygame.font.match_font('arial')
 
 
 def draw_text(surf, text, size, x, y):
@@ -175,6 +180,7 @@ def render(screen, font, number, list_points):
     for i in list_points:
         if number == i[5]:
             screen.blit(font.render(i[2], False, i[4]), (i[0], i[1]))
+
         else:
             screen.blit(font.render(i[2], False, i[3]), (i[0], i[1]))
 
@@ -182,7 +188,7 @@ def render(screen, font, number, list_points):
 def menu_stop(screen, hero):
     pygame.mixer.music.pause()
     screen.fill(BLACK)
-    pause = True
+
     font_menu = pygame.font.Font(None, 50)
     list_points = [(800, 600, 'Продолжить', RED, GRAY, 5),
                    (250, 600, 'Выйти в меню', RED, GRAY, 4),
@@ -207,6 +213,7 @@ def menu_stop(screen, hero):
     text_upgrade_1 = font_menu.render("Постоянные улучшения:", False, (255, 0, 0))
     text_upgrade_2 = font_menu.render("Временные улучшения:", False, (255, 0, 0))
 
+    pause = True
     while pause:
         draw_text(screen, str(souls), 18, WIDTH / 2, 10)
         pygame.draw.rect(screen, RED, (70, 50, 1070, 700), 1)
@@ -219,46 +226,58 @@ def menu_stop(screen, hero):
 
         screen.blit(text_upgrade_1, (80, 100))
         screen.blit(text_upgrade_2, (80, 300))
+
         for i in list_points:
             if i[0] < x < (i[0] + 150) and i[1] < y < (i[1] + 50):
                 point = i[5]
                 break
+
             else:
                 point = -1
+
         render(screen, font_menu, point, list_points)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     if point > 0:
                         point -= 1
+
                 if event.key == pygame.K_RIGHT:
                     if point < len(list_points) - 1:
                         point += 1
+
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if point == 0:
                     global hero_health
                     hero.health += 1
                     hero_health += 1
+
                 elif point == 1:
                     global bulls_damage
                     bulls_damage += 5
+
                 elif point == 4:
                     pygame.mixer.music.unpause()
                     return True
+
                 elif point == 5:
                     pygame.mixer.music.unpause()
                     return False
+
         pygame.display.flip()
 
 
 def draw_health_bar(surf, max_health, health):
     if health < 0:
         health = 0
+
     fill = (health / max_health) * max_health * 50
-    outline_rect = pygame.Rect(5, 5, max_health * 50, 20)
-    fill_rect = pygame.Rect(5, 5, fill, 20)
+    outline_rect = pygame.Rect(5, HEIGHT - 30, max_health * 50, 20)
+    fill_rect = pygame.Rect(5, HEIGHT - 30, fill, 20)
+
     pygame.draw.rect(surf, "#FF1010", fill_rect)
     pygame.draw.rect(surf, WHITE, outline_rect, 2)
 
@@ -291,10 +310,11 @@ def main_game(level, hero):
 
     killed_enemy = 0
     enemy_health = 30
-    enemy_speed = 100
+    enemy_speed = 70
 
     direction_x = ""
     direction_y = ""
+
     running = True
     while running:
         screen.fill(BLACK)
@@ -313,6 +333,7 @@ def main_game(level, hero):
                 if pygame.time.get_ticks() - previous_attack > attack_cooldown:
                     bullet_list.append(Bullet(event.pos, (main_hero.rect.x + main_hero.rect.w // 2,
                                                           main_hero.rect.y + main_hero.rect.h // 2), bulls_damage))
+
                     previous_attack = pygame.time.get_ticks()
 
             if event.type == enemy_upgrade_timer:
@@ -323,12 +344,15 @@ def main_game(level, hero):
                 if side == 0:
                     x = random.randint(10, WIDTH - 10)
                     y = 10
+
                 elif side == 1:
                     y = random.randint(10, HEIGHT - 10)
                     x = 10
+
                 elif side == 2:
                     x = random.randint(10, WIDTH - 10)
                     y = HEIGHT - 10
+
                 else:
                     y = random.randint(10, HEIGHT - 10)
                     x = 10
@@ -356,12 +380,14 @@ def main_game(level, hero):
 
         all_sprites.draw(screen)
         all_sprites.update(main_hero)
+
         if main_hero.is_died:
             break
 
         for index, bullet in enumerate(bullet_list):
             pygame.draw.circle(screen, bullet.color, bullet.coords, bullet.size)
             bullet.move()
+
             if bullet.is_died:
                 bullet_list.pop(index)
 
@@ -372,6 +398,7 @@ def main_game(level, hero):
                     x += random.randint(-20, 20)
                     y += random.randint(-20, 20)
                     SoulsDrop(all_sprites, x, y)
+
                 enemy.kill()
                 enemy_list.remove(enemy)
                 killed_enemy += 1
--- menu.py
+++ menu.py
@@ -1,11 +1,11 @@
 import sys
-
 import pygame
 import pygame_widgets
+
 from pygame_widgets.slider import Slider
 from pygame_widgets.textbox import TextBox
-
 from main import main_game
+
 
 BACKGROUND_COLOR = "#0A0A0A"
 WHITE = "#FFFFFF"
@@ -33,13 +33,16 @@ class Menu:
                                      (500, 300, 'Настройки', RED, GRAY, 1),
                                      (500, 400, 'Правила', RED, GRAY, 2),
                                      (500, 500, 'Выход', RED, GRAY, 3)]
+
         self.list_point_menu_volume = [(100, 600, 'Обратно в меню', RED, GRAY, 0),
                                        (100, 400, 'Мелодия 1', RED, GRAY, 1),
                                        (300, 400, 'Мелодия 2', RED, GRAY, 2),
                                        (500, 400, 'Мелодия 3', RED, GRAY, 3)]
+
         self.list_point_menu_hero = [(250, 600, 'Рифжих', RED, GRAY, 0),
                                      (850, 600, 'Лэйхо', RED, GRAY, 2),
                                      (500, 650, 'Назад', RED, GRAY, 1)]
+
         self.list_point_menu_level = [(150, 300, 'Уровень 1', RED, GRAY, 0),
                                       (850, 300, 'Уровень 2', RED, GRAY, 1),
                                       (150, 600, 'Уровень 3', RED, GRAY, 2),
@@ -53,50 +56,68 @@ class Menu:
         self.music = 'data/game_music_1.mp3'
 
     def menu_volume(self):
+
         screen.fill(BACKGROUND_COLOR)
         pygame.draw.rect(screen, RED, (70, 50, 1070, 700), 1)
+
         slider_menu = Slider(screen, 500, 100, 500, 40, min=0, max=100, step=1,
                              initial=self.menu_vol * 100)
+
+        slider_game = Slider(screen, 500, 200, 500, 40, min=0, max=100, step=1,
+                             initial=self.game_volume * 100)
+
         box_menu = TextBox(screen, 1050, 100, 50, 50, fontSize=30)
-        slider_game = Slider(screen, 500, 200, 500, 40, min=0, max=100, step=1, initial=self.game_volume * 100)
         box_game = TextBox(screen, 1050, 200, 50, 50, fontSize=30)
         box_menu.disable()
         box_game.disable()
-        run_volume = True
+
         render(screen, self.font_menu, 0, self.list_point_menu_volume)
         point = int(self.music.split('.')[0][-1])
         play_game = False
+
+        run_volume = True
         while run_volume:
             events = pygame.event.get()
+
             text_v_menu = self.font_menu.render("Громкость в меню", False, (255, 0, 0))
             screen.blit(text_v_menu, (100, 100))
+
             text_v_game = self.font_menu.render("Громкость в игре", False, (255, 0, 0))
             screen.blit(text_v_game, (100, 200))
+
             text_choose = self.font_menu.render("Выберите мелодию для проигрывания в игре", False, (255, 0, 0))
             screen.blit(text_choose, (100, 300))
+
             x, y = pygame.mouse.get_pos()
             self.menu_vol = slider_menu.getValue() / 100
             self.game_volume = slider_game.getValue() / 100
+
             box_menu.setText(slider_menu.getValue())
             box_game.setText(slider_game.getValue())
+
             pygame_widgets.update(events)
             for i in self.list_point_menu_volume:
                 if i[0] < x < (i[0] + 150) and i[1] < y < (i[1] + 50):
                     point = i[5]
                     break
+
                 else:
                     point = -1
+
             render(screen, self.font_menu, point, self.list_point_menu_volume)
             for event in events:
                 if event.type == pygame.QUIT:
                     sys.exit()
+
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
                         if point > 0:
                             point -= 1
+
                     elif event.key == pygame.K_RIGHT:
                         if point < len(self.list_point_menu_volume) - 1:
                             point += 1
+
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     if point >= 0:
                         if point == 0:
@@ -106,14 +127,17 @@ class Menu:
                             play_game = False
                             run_volume = False
                             screen.fill(BACKGROUND_COLOR)
+
                         else:
                             self.music = f'data/game_music_{point}.mp3'
                             pygame.mixer.music.load(self.music)
                             pygame.mixer.music.set_volume(self.game_volume)
                             pygame.mixer.music.play(-1, 1)
                             play_game = True
+
             if play_game:
                 pygame.mixer.music.set_volume(self.game_volume)
+
             else:
                 pygame.mixer.music.set_volume(self.menu_vol)
 
@@ -122,32 +146,40 @@ class Menu:
 
         slider_menu.hide()
         slider_game.hide()
+
         box_menu.hide()
         box_game.hide()
 
     def menu_hero(self):
         screen.fill(BACKGROUND_COLOR)
-        run_hero = True
         point = -1
+
+        rif = pygame.image.load('data/Рифжих.png')
+        lai = pygame.image.load('data/Лэйхо.png')
+
+        run_hero = True
         while run_hero:
             pygame.draw.rect(screen, RED, (70, 50, 1070, 700), 1)
             x, y = pygame.mouse.get_pos()
-            rif = pygame.image.load('data/Рифжих.png')
+
             image1 = pygame.transform.scale(rif, (200, 200))
             screen.blit(image1, (220, 350))
-            lai = pygame.image.load('data/Лэйхо.png')
+
             image2 = pygame.transform.scale(lai, (200, 200))
             screen.blit(image2, (800, 350))
+
             for i in self.list_point_menu_hero:
                 if i[0] < x < (i[0] + 150) and i[1] < y < (i[1] + 50):
                     point = i[5]
                     break
                 else:
                     point = -1
+
             render(screen, self.font_menu, point, self.list_point_menu_hero)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
                         if point > 0:
@@ -155,35 +187,44 @@ class Menu:
                     if event.key == pygame.K_RIGHT:
                         if point < len(self.list_point_menu_hero) - 1:
                             point += 1
+
                 elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     if point == 0:
                         self.menu_level('Рифжих')
+
                     elif point == 2:
                         self.menu_level('Лэйхо')
+
                     elif point == 1:
                         run_hero = False
+
                     screen.fill(BACKGROUND_COLOR)
                     render(screen, self.font_menu, point, self.list_point_menu_hero)
+
             pygame.display.flip()
 
     def menu_level(self, hero):
         screen.fill(BACKGROUND_COLOR)
-        run_level = True
-        level = ""
         point = -1
+
+        run_level = True
         while run_level:
             pygame.draw.rect(screen, RED, (70, 50, 1070, 700), 1)
             x, y = pygame.mouse.get_pos()
+
             for i in self.list_point_menu_level:
                 if i[0] < x < (i[0] + 150) and i[1] < y < (i[1] + 50):
                     point = i[5]
                     break
+
                 else:
                     point = -1
+
             render(screen, self.font_menu, point, self.list_point_menu_level)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
                         if point > 0:
@@ -191,49 +232,52 @@ class Menu:
                     if event.key == pygame.K_RIGHT:
                         if point < len(self.list_point_menu_level) - 1:
                             point += 1
+
                 elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     if point == 4:
                         run_level = False
+
                     else:
                         pygame.mixer.music.load(self.music)
                         pygame.mixer.music.set_volume(self.game_volume)
                         pygame.mixer.music.play(-1, 2)
-                        if point == 0:
-                            level = '1'
-                        elif point == 1:
-                            level = '2'
-                        elif point == 2:
-                            level = '3'
-                        elif point == 3:
-                            level = '4'
-                        main_game(level, hero)
+
+                        main_game(str(point + 1), hero)
+
                         pygame.mixer.music.load('data/menu_music.mp3')
                         pygame.mixer.music.set_volume(self.menu_vol)
                         pygame.mixer.music.play()
+
                         screen.fill(BACKGROUND_COLOR)
+
             pygame.display.flip()
 
     def menu(self):
-        run = True
-
         point = -1
         pygame.mixer.music.load('data/menu_music.mp3')
         pygame.mixer.music.play(-1)
         pygame.mixer.music.set_volume(0.5)
+
+        run = True
         while run:
             screen.fill(BACKGROUND_COLOR)
+
             pygame.draw.rect(screen, RED, (70, 50, 1070, 700), 1)
             x, y = pygame.mouse.get_pos()
+
             for i in self.list_point_main_menu:
                 if i[0] < x < (i[0] + 150) and i[1] < y < (i[1] + 50):
                     point = i[5]
                     break
+
                 else:
                     point = -1
+
             render(screen, self.font_menu, point, self.list_point_main_menu)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_UP:
                         if point > 0:
@@ -241,16 +285,22 @@ class Menu:
                     if event.key == pygame.K_DOWN:
                         if point < len(self.list_point_main_menu) - 1:
                             point += 1
+
                 elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     if point == 0:
                         self.menu_hero()
+
                     elif point == 1:
                         self.menu_volume()
+
                     elif point == 3:
                         sys.exit()
+
                     elif point == 4:
                         pass
+
                     render(screen, self.font_menu, point, self.list_point_main_menu)
+
             pygame.display.flip()
 
 
